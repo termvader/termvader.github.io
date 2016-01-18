ae68ffc81e427baab69c8e59bf74c4050e1c3273
Aziz Khambati <aziz.h.khambati@gmail.com>
2016-01-18 16:26:06 +0530

    Added attributes and fix some bugs

--- markdown-it-incremental-dom/main.js
+++ markdown-it-incremental-dom/main.js
@@ -4,7 +4,10 @@ var inp = document.getElementById('input');
 var outp = document.getElementById('output');
 var md = markdownit();
 
-var open = IncrementalDOM.elementOpen,
+var openStart = IncrementalDOM.elementOpenStart,
+  openEnd = IncrementalDOM.elementOpenEnd,
+  open = IncrementalDOM.elementOpen,
+  attr = IncrementalDOM.attr,
   close = IncrementalDOM.elementClose,
   elementVoid = IncrementalDOM.elementVoid,
   text = IncrementalDOM.text;
@@ -12,7 +15,7 @@ var open = IncrementalDOM.elementOpen,
 var patch = IncrementalDOM.patch;
 
 function update() {
-  var tokens = md.parse(inp.value);
+  var tokens = md.parse(inp.value, {});
 
   patch(outp, function() {
     renderTokens(tokens);
@@ -22,18 +25,23 @@ function update() {
 function renderTokens(tokens) {
   for (var i=0, len=tokens.length; i<len; i++) {
     var token = tokens[i];
-    if (token.tag) {
-      if (token.nesting === 1) {
-        open(token.tag);
-      } else if (token.nesting === -1) {
-        close(token.tag);
+    if (token.tag && ((token.nesting === 1) || (token.nesting === 0))) {
+      openStart(token.tag);
+      if (token.attrs && token.attrs.length) {
+        for (var j=0, lenj=token.attrs.length; j<lenj; j++) {
+          attr(token.attrs[j][0], token.attrs[j][1]);
+        }
       }
+      openEnd(token.tag);
     }
-    if (token.type === 'text') {
+    if (token.content && (token.type !== 'inline')) {
       text(token.content);
     }
     if (token.children && token.children.length) {
       renderTokens(token.children);
+    }
+    if (token.tag && ((token.nesting === -1) || (token.nesting === 0))) {
+      close(token.tag);
     }
   }
 }
